05602e1c60a321b6c5c8e6383d65dd16cac4d1c5
fantasy <fantasy@VAIO>
2019-12-10 17:30:21 +0800

    fix mybais 3.5.3

---
+++ app-web/src/test/java/app/test2/Test2.java
@@ -0,0 +1,13 @@
+package app.test2;
+
+public class Test2 {
+
+	private static int x = 6;
+	private static int y = 5;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[][] xx = new String[x][y];
+	}
+
+}
